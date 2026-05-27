b18fd4b8f9b079de46fdaef499a3d36fa40edd76
Delucie_coder <d.rurangwa@alustudent.com>
2026-05-27 12:20:10 +0200

    classification

--- supervised_learning/classification/0-neuron.py
+++ supervised_learning/classification/0-neuron.py
@@ -1,20 +1,47 @@
 #!/usr/bin/env python3
 '''
-    Bag of words
+    A class Neuron that defines a single neuron performing
+    binary classification:
 '''
 
 
-from sklearn.feature_extraction.text import CountVectorizer
+import numpy as np
 
 
-
-def bag_of_words(sentences, vocab=None):
+class Neuron:
     '''
-        bag of words
+        Class Neuron
     '''
-    vectorizer = CountVectorizer(vocabulary=vocab)
-    X_train_counts = vectorizer.fit_transform(sentences)
-    embeddings = X_train_counts.toarray()
-    features = vectorizer.get_feature_names()
-    return embeddings, features
+    def __init__(self, nx):
+        '''
+            Constructor
+        '''
+        if type(nx) is not int:
+            raise TypeError('nx must be an integer')
+        if nx < 1:
+            raise ValueError('nx must be a positive integer')
+        self.__W = np.random.randn(1, nx)
+        self.__b = 0
+        self.__A = 0
+
+    @property
+    def W(self):
+        '''
+            Getter
+        '''
+        return self.__W
+
+    @property
+    def b(self):
+        '''
+            Getter
+        '''
+        return self.__b
+
+    @property
+    def A(self):
+        '''
+            Getter
+        '''
+        return self.__A
     
